8ea46685fcdbe74d16e00cdb769f8b1342d26aa2
Arturo Cuya <arturocuya099@gmail.com>
2022-04-11 22:38:09 -0500

    feat(model): wip: leave ready throw statement for validateArgs failure
    this can't be implemented rn because brighterscript does not support `ThrowStatement` as an argument
    for `createVisitor`, which doesn't allow us to fully implement the `@deviceOnly` annotation

--- bsconfig/plugins/testAnnotations.ts
+++ bsconfig/plugins/testAnnotations.ts
@@ -1,4 +1,4 @@
-import type { BeforeFileTranspileEvent, CompilerPlugin } from 'brighterscript';
+import type { BeforeFileTranspileEvent, CompilerPlugin, Statement } from 'brighterscript';
 import { isBrsFile, createVisitor, WalkMode } from 'brighterscript';
 
 /**
@@ -14,19 +14,11 @@ export default function plugin() {
         beforeFileTranspile: (event: BeforeFileTranspileEvent) => {
             if (isBrsFile(event.file)) {
                 for (const func of event.file.parser.references.functionExpressions) {
-                    // TODO: Ask Bronley how to affect all statements and substatements.
-                    // TODO: What about try/catch/throw?
                     func.body.walk(createVisitor({
-                        DottedSetStatement: (statement) => {
-                            if (statement.annotations?.find(a => a.name === 'deviceOnly')) {
-                                event.editor.overrideTranspileResult(statement, '');
-                            }
-                        },
-                        ExpressionStatement: (statement) => {
-                            if (statement.annotations?.find(a => a.name === 'deviceOnly')) {
-                                event.editor.overrideTranspileResult(statement, '');
-                            }
-                        }
+                        DottedSetStatement: (statement) => handleDeviceOnlyAnnotation(statement, event),
+                        ExpressionStatement: (statement) => handleDeviceOnlyAnnotation(statement, event)
+                        // TODO: Uncomment this when `ThrowStatement` is supported as an argument for `createVisitor`
+                        // ThrowStatement: (statement) => handleDeviceOnlyAnnotation(statement, event)
                     }), {
                         walkMode: WalkMode.visitStatements
                     });
@@ -36,30 +28,8 @@ export default function plugin() {
     } as CompilerPlugin;
 }
 
-/*
-Advice to find what kind of statement you should override:
-1. Reduce the scope of your plugin to the file you are interested in.
-
-```
-if (isBrsFile(event.file) && event.file.pkgPath.includes('SGFlexModel')) {
-```
-
-2. Go through all the functionExpressions' functionStatements
-(limiting yourself to the function you are interested in) and check for
-body statements with annotations.
-
-```
-for (const s of event.file.parser.references.functionExpressions) {
-    if (s.functionStatement.name.text === 'validateDirection') {
-        for (const bodyStatement of s.body.statements) {
-            if (bodyStatement.annotations) {
-                console.log(bodyStatement);
-            }
-        }
+function handleDeviceOnlyAnnotation(statement: Statement, event: BeforeFileTranspileEvent) {
+    if (statement.annotations?.find(a => a.name === 'deviceOnly')) {
+        event.editor.overrideTranspileResult(statement, '');
     }
 }
-```
-
-3. The first line of the console log will tell you the type of the statement
-you want to override.
-*/
